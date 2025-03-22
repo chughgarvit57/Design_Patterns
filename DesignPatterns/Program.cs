a0c0c70fc6203a19cb74e7a93aa2fe154d11c172
chughgarvit57 <gavi.chugh@gmail.com>
2025-03-22 14:03:54 +0530

    Singleton Design Pattern Implemented

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -5,6 +5,22 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Design Patterns Tutorial!");
+            Parallel.Invoke(
+                () => PrintEmployeeDetails(),
+                () => PrintStudentDetails()
+                );
+        }
+
+        private static void PrintEmployeeDetails()
+        {
+            Singleton fromEmployeeClass = Singleton.GetInstance;
+            fromEmployeeClass.PrintMessage("Suppose From Employee Class");
+        }
+
+        private static void PrintStudentDetails()
+        {
+            Singleton fromStudentClass = Singleton.GetInstance;
+            fromStudentClass.PrintMessage("Suppose From Student Class");
         }
     }
 }
---
+++ DesignPatterns/Singleton.cs
@@ -0,0 +1,24 @@
+﻿namespace DesignPatterns
+{
+    public sealed class Singleton
+    {
+        private static int count = 0;
+        private static readonly Lazy<Singleton> instance = new Lazy<Singleton>(() => new Singleton());
+        public static Singleton GetInstance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+        private Singleton()
+        {
+            count++;
+            Console.WriteLine($"Count: {count}");
+        }
+        public void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
